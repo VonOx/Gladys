4d520f875813a0ec167e646bbd0b17f6e0f9f61b
VonOx <vkulak62@gmail.com>
2019-04-07 12:01:16 +0200

    Test

--- .circleci/build-image.sh
+++ .circleci/build-image.sh
@@ -14,7 +14,7 @@ export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${TAG}"
 # Replace the repo's Dockerfile with our own.
 docker build -t ${IMAGE_ID} \
   --build-arg target=$TARGET \
-  --build-arg arch=$QEMU_ARCH ./docker/Dockerfile.${TARGET} .
+  --build-arg arch=$QEMU_ARCH -f ./docker/Dockerfile.${TARGET} .
 
 # Login to Docker Hub.
 echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin
