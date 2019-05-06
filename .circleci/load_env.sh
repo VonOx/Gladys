714cb4f47b8435f82b71959dd21e9b719b5316b0
VonOx <vkulak62@gmail.com>
2019-05-06 14:28:48 +0200

    Another method ci

--- .circleci/load_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo 'export GITHUB_REPO=VonOx/Gladys' >> $BASH_ENV
-echo 'export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases/latest | jq -r ".tag_name")' >> $BASH_ENV
-echo 'export REGISTRY=vonox' >> $BASH_ENV
-echo 'export IMAGE=gladys' >> $BASH_ENV
-echo 'export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${TAG}"' >> $BASH_ENV
-echo 'export DIR=`pwd`' >> $BASH_ENV
-echo 'export QEMU_VERSION="v4.0.0"' >>$BASH_ENV
-. $BASH_ENV
